dc168469533ef202401fe159bc878dbfa5321cfa
Ananya0511 <dananya028@gmail.com>
2022-12-19 16:43:31 +0530

    coin+lineChart+selectDays

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
-import Footer from './components/Common/Footer/footer';
-import Header from './components/Common/Header/header';
-import LandingPageComponent from './components/LandingPage/Intro';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from './pages/home';
 import DashboardPage from './pages/dashboard';
+import CoinPage from './pages/coin';
 
 function App() {
   return (
@@ -13,11 +11,9 @@ function App() {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/dashboard" element={<DashboardPage />} />
+            <Route path="/coin/:id" element={<CoinPage />} />
           </Routes>
         </BrowserRouter>
-      {/* <Header/> */}
-      {/* <LandingPageComponent/> */}
-      {/* <Footer/> */}
     </div>
   );
 }
