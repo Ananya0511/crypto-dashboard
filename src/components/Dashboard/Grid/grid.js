26eb9baa4de430fcb5c5f238feeef9fa8d5b7fbd
Ananya0511 <dananya028@gmail.com>
2022-12-30 23:36:46 +0530

    Completely Responsive

--- src/components/Dashboard/Grid/grid.js
+++ src/components/Dashboard/Grid/grid.js
@@ -1,56 +1,3 @@
-// import React from 'react'
-// import "./style.css"
-// import TrendingUpIcon from '@mui/icons-material/TrendingUp';
-// import TrendingDownIcon from '@mui/icons-material/TrendingDown';
-// import { motion } from "framer-motion";
-
-// function Grid({ coin, delay }) {
-//   return (
-//     <a href={`/coin/${coin.id}`}>
-//     <motion.div 
-//     initial={{ opacity: 0, y: 50 }}
-//     whileInView={{ opacity: 1, y: 0 }}
-//     transition={{ duration: 0.5, delay: delay}}
-//     className={`grid-container ${coin.price_change_percentage_24h < 0 && "grid-container-red"
-//     }`}>
-//         <div className='info-flex'>
-//             <img src={coin.image} className="coin-image" alt='coin'/>
-//             <div className='coin-name-flex'>
-//                 <h3 className='coin-symbol'>{coin.symbol}</h3>
-//                 <p className='coin-name'>{coin.name}</p>
-//             </div>
-//         </div>
-
-//         {coin.price_change_percentage_24h > 0 ? (
-//             <div className='info-flex'>
-//                 <div className='price-chip'>
-//                     {coin.price_change_percentage_24h.toFixed(2)} %
-//                 </div>
-//             <TrendingUpIcon className="trending-icon" />
-//             </div>
-//         ) : (
-//             <div className='info-flex'>
-//                 <div className='price-chip red'>
-//                     {coin.price_change_percentage_24h.toFixed(2)} %
-//             </div>
-//             <TrendingDownIcon className="trending-icon red" />
-//             </div>
-//         )}
-//         <p className={`coin-price 
-//         ${coin.price_change_percentage_24h < 0 && "coin-price-red"
-//         }`}
-//         >
-//             ${coin.current_price.toLocaleString()}
-//         </p>
-//         <p className='coin-name-2'>Total Volume: <span className='coin-total_volume'>{coin.total_volume.toLocaleString()}</span></p>
-//         <p className='coin-name-2'>Market Cap: <span className='coin-total_volume'>${coin.market_cap.toLocaleString()}</span></p>
-//     </motion.div>
-//     </a>
-//   )
-// }
-
-// export default Grid;
-
 import React, { useState } from "react";
 import "./style.css";
 import TrendingUpRoundedIcon from "@mui/icons-material/TrendingUpRounded";
@@ -79,7 +26,7 @@ function Grid({ coin, delay, isWatchlistPage }) {
       >
         <div className="info-flex">
           <div className="coin-info-flex">
-            <img src={coin.image} className="coin-image" />
+            <img src={coin.image} className="coin-image" alt="coin"/>
             <div className="coin-name-flex">
               <h3 className="coin-symbol">{coin.symbol}</h3>
               <p className="coin-name">{coin.name}</p>
