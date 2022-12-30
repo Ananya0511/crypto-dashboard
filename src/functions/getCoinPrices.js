37bfde286a106c721da33c8dfcc5d9142839339a
Ananya0511 <dananya028@gmail.com>
2022-12-30 21:54:34 +0530

    SEO+Constants file

--- src/functions/getCoinPrices.js
+++ src/functions/getCoinPrices.js
@@ -1,14 +1,15 @@
 import axios from "axios";
+import { API_URL } from "../constants";
 
 export const getCoinPrices = (id, days, priceType) => {
     const prices = axios
       .get(
-        `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=${days}&interval=daily`
+        `${API_URL}/${id}/market_chart?vs_currency=usd&days=${days}&interval=daily`
       )
       .then((response) => {
-        if (priceType == "market_caps") {
+        if (priceType === "market_caps") {
           return response.data.market_caps;
-        } else if (priceType == "total_volumes") {
+        } else if (priceType === "total_volumes") {
           return response.data.total_volumes;
         } else {
           return response.data.prices;
